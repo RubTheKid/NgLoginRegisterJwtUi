02d24ed7ded9dc7ee3bd891828dc934ab3f481b0
RubTheKid <you@example.com>
2023-03-28 10:48:35 -0300

    update aaaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgLoginRegisterJwtUi';
+  user = new User();
+
+  constructor(private AuthService: AuthService){}
+
+  register(user: User){
+    this.AuthService.register(user).subscribe();
+  }
+
+  login(user: User){
+    this.AuthService.login(user).subscribe((token: string) => {
+      localStorage.setItem('authToken', token);
+    });
+  }
+
+  getusername(){
+    this.AuthService.getUserName().subscribe((name: string) =>{
+      console.log(name);
+    });
+  }
+
+
+
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  public register (user: User): Observable<any> {
+    return this.http.post<any>(
+      'https://localhost:7164/api/Auth/register',
+      user
+      );
+  }
+
+  public login (user: User): Observable<any>{
+    return this.http.post(
+      'https://localhost:7164/api/Auth/register',
+      user, { responseType: 'text' }
+    );
+  }
+
+  public getUserName(): Observable<string>{
+    return this.http.get<string>('https://localhost:7164/api/Auth/');
+  }
+  
+  
+
+}
