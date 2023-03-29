fa7ec78c36faacbb65050076c6b33a0053369d91
RubTheKid <you@example.com>
2023-03-29 17:52:16 -0300

    update

--- src/app/services/home/home.component.ts
+++ src/app/services/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private AuthService: AuthService) { }
 
   ngOnInit(): void {
   }
-
+  getusername(){
+    this.AuthService.getUserName().subscribe((name: string) =>{
+      console.log(name);
+      
+    });
+  }
 }
